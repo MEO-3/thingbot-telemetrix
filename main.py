760c71fd1781a2a23fea6c748f638f9b7c4131c9
lgthvinh <everwellmax@gmail.com>
2026-02-09 02:04:19 +0700

    Remove unused LED control loop and import statements from main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import time
-import sys
-
 from thingbot_telemetrix import telemetrix
 from thingbot_telemetrix.private_constants import ThingBot
 
 board = telemetrix.Telemetrix()
 
 board.thingbot().control_led(ThingBot.LED_1, 100)
-
-while True:
-    time.sleep(2)
-    board.thingbot().control_led(ThingBot.LED_2, 0)
-    print("LED OFF")
-    time.sleep(2)
-    board.thingbot().control_led(ThingBot.LED_2, 100)
-    print("LED ON")
